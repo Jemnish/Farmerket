83d93337f79665507f25e61aed0ea18677bc8b83
Jenish Budhathoki <Budathokizone111@gmail.com>
2025-01-28 19:00:25 +0545

    user block on multiple failed attempts added

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const nav_links = [
-  { path: "/", display: "Home" },
+  { path: "/home", display: "Home" },
   { path: "/about", display: "About" },
   { path: "/marketplace", display: "Marketplace" },
   { path: "/contact", display: "Contact" },
